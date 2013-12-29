0bf3e0216d1528c404b6592aa8f5ce57f3871dd1
David Villegas <dvilleg@us.ibm.com>
2013-12-28 19:56:34 -0500

    Generate candidates, implement tournament

--- GAPaint.hs
+++ GAPaint.hs
@@ -3,22 +3,38 @@
 module GAPaint where
 
 import Bitmap (Triangle(..), Vertex(..), Color(..))
+import Render (trianglesToBitmap)
 
 import System.Random
+import Control.Monad (replicateM)
+import Data.Word (Word8)
+import Data.List (maximumBy)
+---------------------------------------------------
+-- GA Parameters
 
--- evolve :: IO ()
--- evolve = do contents <- readPNM "mona.pnm"
---            writePNM "mona2.pnm" contents
-         
+initialPop = 100 -- Size of the initial candidate pool
 
--- |A Pool is a list of candidates
--- type Pool = [Candidate]
-  
+minTriangles = 1  -- Minimum number of triangles for initial candidate
+maxTriangles = 10 -- Maximum number of triangles for initial candidate
 
+---------------------------------------------------
+
+-- |A candidate has a list of triangles
+type Candidate = [Triangle]
+
+-- |A candidate's Score is its fitness (the lower the better)
+data Score = Score {
+  candidate :: Candidate,
+  fitness :: Double
+  }
 
 -- |Generate a random Integer between lo and hi, both included
 randInt :: Int -> Int -> IO Int
 randInt lo hi = getStdRandom (randomR (lo,hi))
+
+-- |Generate a random Double between lo and hi, both included
+randD :: Double -> Double -> IO Double
+randD lo hi = getStdRandom (randomR (lo,hi))
 
 -- |Generate a random alpha value between 0 and 255
 randomAlpha :: IO Int
@@ -51,13 +67,54 @@ randomTriangle (w,h) = do
   a <- randomAlpha
   return $ Triangle (v1,v2,v3) c a
 
+-- |Generate one candidate with random triangles
+generateCandidate :: (Int,Int) -> IO Candidate
+generateCandidate (w,h) = do
+  nTriangles <- randInt minTriangles maxTriangles
+  replicateM nTriangles (randomTriangle (w,h))
 
--- generate initial pool of size s
--- -- each candidate has a # of triangles between n m
+-- |Generate initialPop # of candidates
+generatePool :: (Int,Int) -> IO [Candidate]
+generatePool (w,h) = replicateM initialPop (generateCandidate (w,h))
+
+-- |Given a target picture and a candidate, return the score
+score :: [Word8] -> Candidate -> (Int,Int) -> IO Score
+score target candidate (w,h) = do
+  pixels <- trianglesToBitmap candidate (w,h)
+  return $ Score candidate (fitnessF target pixels)
+
+-- |The fitness function
+fitnessF :: [Word8] -> [Word8] -> Double
+fitnessF t c = total / (fromIntegral (length t))
+  where absum x y = if x > y then x - y else y - x
+        total = sum $ map fromIntegral $ zipWith absum t c
+
+-- |Generate a list of n unique indices between 0 and max (inclusive), making
+-- sure that they are not repeated
+generateUniqueIndices :: Int -> Int -> IO [Int]
+generateUniqueIndices n max = genUniq n []
+  where genUniq 0 is = return is
+        genUniq n is = do
+          i <- randInt 0 max
+          if (i `elem` is) then genUniq n is else genUniq (n-1) (i:is)
+
+-- |Tournament selection: from a pool of n individuals, pick 'tourSize' of
+-- them. Out of those, the one that survives is the one with the best fitness
+-- score. Repeat until there are 'genSize' results
+tournament :: [Score] -> Int -> Int -> IO [Score]
+tournament scores genSize tourSize = tournament' scores genSize tourSize []
+    where scoreCmp (Score _ f1) (Score _ f2) = compare f2 f1 -- look for min!
+          tournament' sc 0 ts res = return res
+          tournament' sc gs ts res = do
+            indices <- generateUniqueIndices ts ((length sc)-1)
+            let winner = maximumBy scoreCmp $ map (sc !!) indices
+            tournament' sc (gs-1) ts (winner:res)
+
+
 
 -- crossover phase (s times):
 -- -- select 2 parents based on highest fitness
--- -- if random(pcross) 
+-- -- if random(pcross)
 -- -- -- crossover to obtain 2 children
 -- -- else:
 -- -- -- clone
@@ -80,6 +137,28 @@ randomTriangle (w,h) = do
 -- -- else: remove a triangle
 
 -- fitness:
--- -- given a target image with pixels p@(r,g,b)... and a candidate with 
--- -- p2@(r2,g2,b2), fitness is = wsim * abs(p-p2) + wsiz * # triangles, where 
+-- -- given a target image with pixels p@(r,g,b)... and a candidate with
+-- -- p2@(r2,g2,b2), fitness is = wsim * abs(p-p2) + wsiz * # triangles, where
 -- -- p-p2 = (r-r2+g-g2+b-b2)
+
+
+-----------------------
+---- Can't use roulette for a minimization problem...
+-- accumPick :: Double -> [(Double, Score)] -> Score
+-- accumPick choice ss = accumPick' 0 ss
+--   where accumPick' n ((p,s):ss) = if choice <= (n+p)
+--                                   then s else accumPick' (n+p) ss
+
+-- -- |Roulette: given an array of (candidate,fitness) pairs, choose a candidate
+-- -- proportionally to its fitness. Choice must be a number between 0 and 1
+-- -- t - x / t * (n-1)
+-- roulette :: Double -> [Score] -> Score
+-- roulette choice candidates = let
+--   tot = sum $ map fitness candidates
+--   len = fromIntegral(length candidates)
+--   probs = map (\s@(Score _ f) -> ((tot - f) / (tot * (len-1)), s)) candidates
+--   --sortedScores = sortBy cmpProb probs
+--   --cmpProb (p1, _) (p2, _) = compare p1 p2
+--   -- invScores = map (\s@(Score _ f) -> (totalFitness - f, s)) candidates
+--   in
+--    accumPick choice probs
